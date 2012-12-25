040cbf86c57517f4d5428df3834b1c477a961665
Tao Xie <txie@Fallbrook.local>
2012-12-25 12:16:45 -0800

    block

---
+++ APIDemo/APIDemo/SpecialOps.h
@@ -0,0 +1,13 @@
+//
+//  SpecialOps.h
+//  APIDemo
+//
+//  Created by Tao Xie on 12/25/12.
+//  Copyright (c) 2012 Tao Xie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SpecialOps : NSObject
+
+@end
---
+++ APIDemo/APIDemo/SpecialOps.m
@@ -0,0 +1,13 @@
+//
+//  SpecialOps.m
+//  APIDemo
+//
+//  Created by Tao Xie on 12/25/12.
+//  Copyright (c) 2012 Tao Xie. All rights reserved.
+//
+
+#import "SpecialOps.h"
+
+@implementation SpecialOps
+
+@end
